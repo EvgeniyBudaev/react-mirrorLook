43c025dbe066ee385cc9bd9fcae3d724c0c38928
EvgeniyBudaev <49036840+EvgeniyBudaev@users.noreply.github.com>
2020-12-30 09:36:49 +0300

    add pagination

--- src/frontend/components/paginationUI/paginationUI.jsx
+++ src/frontend/components/paginationUI/paginationUI.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Pagination from '@material-ui/lab/Pagination';
+import styles from '@/frontend/components/paginationUI/paginationUI.module.scss'
 
 const useStyles = makeStyles((theme) => ({
   root: {
     '& > *': {
       marginTop: theme.spacing(2),
+    },
+    '& .MuiPagination-ul': {
+      justifyContent: 'center',
+    },
+    '& .MuiPaginationItem-root': {
+      fontFamily: 'Montserrat',
+      fontSize: '16px',
+    },
+    '& .MuiPaginationItem-outlined': {
+      border: '1px solid #B0976A',
+    },
+    '& .MuiPaginationItem-rounded': {
+      borderRadius: '0px',
+    },
+    '& .MuiPaginationItem-page.Mui-selected': {
+      backgroundColor: '#B0976A',
+      color: '#FFFFFF',
+    },
+    '& .MuiPaginationItem-page:hover': {
+      backgroundColor: '#dfd5c3',
     },
   },
 }));
@@ -14,8 +35,10 @@ const PaginationUI = () => {
   const classes = useStyles();
 
   return (
-    <div className={classes.root}>
-      <Pagination count={10} variant="outlined" shape="rounded" />
+    <div className={styles.pagination}>
+      <div className={classes.root}>
+        <Pagination count={10} variant="outlined" shape="rounded" />
+      </div>
     </div>
   );
 }
